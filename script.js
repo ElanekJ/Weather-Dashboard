a9e69536916fc4f8f4a3cd3db18db24b5a367920
unknown <eliasjozef01@gmail.com>
2023-01-27 23:53:54 +0000

    adding fetch to JAvaScript

--- script.js
+++ script.js
@@ -7,15 +7,19 @@
 
 fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=566a2801aa6d2ed7467b0b0d21c0d5c2")
     .then(response => response.json())
-    .then(data => {
+    .then(citiesFound => {
 
-        console.log(data);
+        let firstCity = citiesFound[0];
+        console.log(firstCity.lat);
+        console.log(firstCity.lon);
+
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=566a2801aa6d2ed7467b0b0d21c0d5c2`)
     })
 
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=566a2801aa6d2ed7467b0b0d21c0d5c2")
+
     .then(response => response.json())    
     .then(data =>{
 
-        console.log(data);
+        console.log(data.city);
     })
