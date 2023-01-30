658e62949e9c30e6d4fb77d0b0bbbb53503c71b2
ElanekJ <eliasjozef01@gmail.com>
2023-01-30 18:54:35 +0000

    creating buttons in jS, creating style.css

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
 
+$(document).ready(function() {
+
+let cities = ["Berlin", "Paris","Edinburgh","Madrid","Birmingham","London"];
+for (let i = 0; i < cities.length; i++) {
+
+    let cityBtn = $("<button>");
+    cityBtn.addClass("city-button city-button-color");
+    cityBtn.text(cities[i]);
+    $("#history").append(cityBtn);
+}
+})
 
 
 
+$("button").on("click", function(){
 
-
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=566a2801aa6d2ed7467b0b0d21c0d5c2")
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=566a2801aa6d2ed7467b0b0d21c0d5c2")
     .then(response => response.json())
     .then(citiesFound => {
 
@@ -21,5 +32,7 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=566a2
     .then(response => response.json())    
     .then(data =>{
 
-        console.log(data.city);
+        console.log(data);
     })
+
+})
