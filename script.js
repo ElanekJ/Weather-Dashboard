193d827ae49b40702be362fb7b771e2ef5f71b76
unknown <eliasjozef01@gmail.com>
2023-01-31 00:25:26 +0000

    set up current day wit temp humi wind day

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+ $(document).ready(function() {
+
+        let cities = ["Berlin", "Paris","Edinburgh","Madrid","Birmingham","London"];
+        for (let i = 0; i < cities.length; i++) {
+        
+            let cityBtn = $("<button>");
+            cityBtn.addClass("city-button city-button-color");
+            cityBtn.text(cities[i]);
+            $("#history").append(cityBtn);
+        }
+        
 
 
-$(document).ready(function() {
+let choseUnit = "&units=metric";
+let timeEl = moment().format("DD/MM/YYYY")
 
-let cities = ["Berlin", "Paris","Edinburgh","Madrid","Birmingham","London"];
-for (let i = 0; i < cities.length; i++) {
+$("button").click(function(event){
 
-    let cityBtn = $("<button>");
-    cityBtn.addClass("city-button city-button-color");
-    cityBtn.text(cities[i]);
-    $("#history").append(cityBtn);
-}
-})
+    let cityEl = event.target.textContent;
+    console.log("cityEl  =" + cityEl)
+    
+
+    let dataCityEl = $(event.target);
+    localStorage.setItem("City" , cityEl);
+    document.querySelector(".card-title").textContent = (localStorage.getItem("City")) + "   ( " + timeEl + " )";
 
 
-
-$("button").on("click", function(){
-
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=566a2801aa6d2ed7467b0b0d21c0d5c2")
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityEl}&limit=5&appid=566a2801aa6d2ed7467b0b0d21c0d5c2`)
     .then(response => response.json())
     .then(citiesFound => {
 
@@ -24,7 +33,7 @@ $("button").on("click", function(){
         console.log(firstCity.lat);
         console.log(firstCity.lon);
 
-        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=566a2801aa6d2ed7467b0b0d21c0d5c2`)
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=566a2801aa6d2ed7467b0b0d21c0d5c2${choseUnit}`)
     })
 
 
@@ -32,7 +41,24 @@ $("button").on("click", function(){
     .then(response => response.json())    
     .then(data =>{
 
-        console.log(data);
-    })
+        let tempEl = "Temp " + data.list[0].main.temp +"C"
 
-})
+       
+        document.querySelector(".card-title").textContent = cityEl + "   ( " + timeEl + " )";
+        document.querySelector(".card-subtitle").innerHTML = "<img src=\"http://openweathermap.org/img/wn/02n@2x.png\">"
+        document.querySelector(".temp").textContent = tempEl
+        document.querySelector(".wind").textContent = "Wind: " + data.list[0].wind.speed + "KPH"
+        document.querySelector(".humidity").textContent = "Humidity: " + data.list[0].main.humidity + "%"
+
+        console.log(data);
+
+        console.log(data.list[0].weather[0].icon);
+        console.log("C ", data.list[0].main.temp, " C ");
+        console.log("Wind ",data.list[0].wind.speed,"KPH");
+        console.log("Humidyty ",data.list[0].main.humidity , "%");
+
+    }) 
+ }) 
+ document.querySelector(".card-title").textContent = (localStorage.getItem("City")) + "   ( " + timeEl + " )";
+
+ })
